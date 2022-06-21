845f5d4b6989fdbd7dbf09d5664f0b826a4ba3e3
Rakesh Prasad <89901373+RakeshPrasad21@users.noreply.github.com>
2022-06-21 18:20:30 +0530

    Update Exitnodes.ps1

--- Exitnodes.ps1
+++ Exitnodes.ps1
@@ -1,32 +1,29 @@
 $DownloadCenterURL = "https://check.torproject.org/torbulkexitlist"
-#$Response = Invoke-WebRequest -URI $DownloadCenterURL
-#$DownloadLink = $Response.Links |  where {$_.href.Contains("/ServiceTags_Public")} | select href
-#$DownloadURL = [System.Uri]$DownloadLink[0].href
-#$jsonFileData = Invoke-WebRequest -Method Get -URI $DownloadURL | ConvertFrom-Json
-#$jsonContent = $jsonFileData | ConvertTo-Json -depth 100
-#$jsonFileData | ConvertTo-Json -depth 100 | Out-File ".\servicetags.json"
+$Response = Invoke-RestMethod -URI $DownloadCenterURL
+$csvFileData = $Response
+
 write-host "File Fetch completed."
 
 #decleration
-$file = ".\Torexitnodes.csv"
-$text = $jsonFileData 
+$file = ".\Torexitnodes.txt"
+#$text = $txtFileData
 $wi = "#13 #14"
 
 "Set config"
-git config --global user.email "builduser@shivam.local" # any values will do, if missing commit will fail
+git config --global user.email "builduser@samik.local" # any values will do, if missing commit will fail
 git config --global user.name "Build user"
 
 "Select a branch"
-git checkout main 2>&1 | write-host # need the stderr redirect as some git command line send none error output here
+git checkout main 2>&1 | write-host # need the stderr redirect as some git command line send none error output here and remove this later.
 
 "Update the local repo"
 git pull  2>&1 | write-host
+
 "Status at start"
 git status 2>&1 | write-host
 
 "Update the file $file"
-//Add-Content -Path $file -Value $jsonContent 
-Set-Content -Path $file -Value (Invoke-WebRequest 'https://check.torproject.org/torbulkexitlist').Content -Encoding Byte
+Add-Content -Path $file -Value $text
 
 "Status prior to stage"
 git status 2>&1 | write-host
